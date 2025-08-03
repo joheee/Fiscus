d83fb2761828cc19a67f111d83aadb3f8ca308de
joheee <johevinblesstowi07@gmail.com>
2025-08-04 05:53:39 +0700

    feat: adjust layout auth

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -27,7 +27,7 @@ export default async function AuthLayout({
     <main className="h-screen flex items-center justify-center bg-blue-accent p-6">
       <div
         className="sm:w-full sm:max-w-md sm:h-fit 
-        rounded-lg p-6 bg-background shadow-md w-screen h-full"
+        rounded-lg p-6 bg-background shadow-md w-screen"
       >
         {children}
       </div>
